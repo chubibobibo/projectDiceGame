4eb5f449e8c1a71b0ab0e8d328a81d6b730f9405
Alistair Abao <abaolester@gmail.com>
2022-09-14 01:23:19 +0200

    changes in app.js
    fixed not resetting when die rolls 1

--- app.js
+++ app.js
@@ -1,205 +1,161 @@
+const randomRoll = () =>
+    Math.floor(Math.random() * 6) + 1
+const roll = randomRoll()
 
-const totalScore1 = document.querySelector('#totalScore1')
-const currentScore1 = document.querySelector('#scoreBox1')
+const player1 = {
+    totalScore: document.querySelector('#totalScore1'),
+    currentScore: document.querySelector('#scoreBox1'),
+    currentArr: [],
+    totalArr: []
+}
 
-const totalScore2 = document.querySelector('#totalScore2')
-const currentScore2 = document.querySelector('#scoreBox2')
+const player2 = {
+    totalScore: document.querySelector('#totalScore2'),
+    currentScore: document.querySelector('#scoreBox2'),
+    currentArr: [],
+    totalArr: []
+
+}
 
 const rollDice = document.querySelector('#rollDice');
-const holdValue = document.querySelector('#holdValue');
-const newGame = document.querySelector('#newGame')
+const holdValueBtn = document.querySelector('#holdValue');
+const newGameBtn = document.querySelector('#newGame')
 let dice = document.querySelector('#dice')
 let message = document.querySelector('#message')
-
-// save random in a function to have it execute random nums
-randomRoll = function () {
-    return Math.floor(Math.random() * 6) + 1
-}
-// let randomRoll = Math.floor(Math.random() * 6) + 1
 let isMyTurn = true;
 let isGameOver = false;
-let currentArr1 = []
-let totalArr1 = []
-let currentArr2 = []
-let totalArr2 = []
 
-
-newGame.addEventListener('click', function () {
-    console.log('clicked')
-    currentScore1.textContent = 0
-    totalScore1.textContent = 0
-    currentArr1 = []
-    totalArr1 = []
-    currentScore2.textContent = 0
-    totalScore2.textContent = 0
-    currentArr2 = []
-    totalArr2 = []
-    isMyTurn = true
-    isGameOver = false
-    totalScore1.classList.remove('winner')
-    totalScore1.classList.remove('loser')
-    totalScore2.classList.remove('winner')
-    totalScore2.classList.remove('loser')
-    message.textContent = ''
-
-})
-
-//  refactored to a function for reusability
-//need to fix the gameover logic
-function updateScore() {
-    if (randomRoll() === 1 && isMyTurn) {
+function reset(player, opponent) {
+    if (player.currentArr.includes(1) === true && isMyTurn === true) {
         // rolling a 1 will result in an empty array and a 0 currentScore text
         //should execute player 2 after
-        currentArr1 = []
-        currentScore1.textContent = '0'
-        console.log(currentArr1)
+        player.currentArr = []
+        player.currentScore.textContent = '0'
+        console.log(player.currentArr)
         isMyTurn = false
-        alert('player1 rolls 1')
+        alert(`player 1 rolled 1`)
+        console.log(randomRoll())
+        console.log(roll)
+        // to keep track of array
+        // console.log('player rolls 1')
+    } else if (player.currentArr.includes(1) === true && isMyTurn === false) {
+        // rolling a 1 will result in an empty array and a 0 currentScore text
+        //should execute player 2 after
+        player.currentArr = []
+        player.currentScore.textContent = '0'
+        console.log(player.currentArr)
+        isMyTurn = true
+        alert(`player 1 rolled 1`)
         // to keep track of array
         // console.log('player rolls 1')
     }
+}
+
+function updateScore(player, opponent) {
     //update the currentArr1 and the currentScore1
     //currentScore1 needs to be totaled to use as the currentScore text
-    else if (isMyTurn === true) {
-        currentArr1.push(randomRoll())
-        // reduce currentArr1 to display total to curentScore1
-        currentScore1.textContent = currentArr1.reduce((first, second) => {
-            return first + second
-        })
-        console.log(currentArr1)
-    }
-}
+    player.currentArr.push(randomRoll())
+    // reduce currentArr1 to display total to curentScore1
+    player.currentScore.textContent = player.currentArr.reduce((first, second) => {
+        return first + second
+    })
+    console.log(player.currentArr)
 
-function udpateScore2() {
-
-    if (randomRoll() === 1 && !isMyTurn) {
-        // rolling a 1 will result in an empty array and a 0 currentScore text
-        //should execute player 1 after
-        currentArr2 = []
-        currentScore2.textContent = '0'
-        isMyTurn = true
-        alert('player 2 rolls 1')
-        console.log('player 2 rolls 1')
-    }
-    //update the currentArr2 and the currentScore2
-    //currentScore2 needs to be totaled to use as the currentScore text
-    else if (isMyTurn === false) {
-        currentArr2.push(randomRoll())
-        // reduced currentArr2 to display total to curentScore1
-        currentScore2.textContent = currentArr2.reduce((first, second) => {
-            return first + second
-        })
-        console.log(currentArr2)
-    }
 
 }
 
-function holdValue1() {
-    currentScore1.textContent = '0'
+function holdValue(player, opponent) {
+    player.currentScore.textContent = '0'
     // save the value of the reduced currentArr1 to totalArr1
-    totalArr1.push(currentArr1.reduce((first, second) => {
+    player.totalArr.push(player.currentArr.reduce((first, second) => {
         return first + second
     }))
     // then use the reduced totalArr1 as the totalScore1
-    totalScore1.textContent = totalArr1.reduce((first, second) => {
+    player.totalScore.textContent = player.totalArr.reduce((first, second) => {
         return first + second
     })
-    currentArr1 = []
+    player.currentArr = []
     // prevent from continuing to play after holdValue is pressed
     isMyTurn = false
-    console.log('player1 holds the  value')
-
-}
-function holdValue2() {
-    currentScore2.textContent = '0'
-    // save the value of the reduced currentArr2 to totalArr2
-    totalArr2.push(currentArr2.reduce((first, second) => {
-        return first + second
-    }))
-    totalScore2.textContent = totalArr2.reduce((first, second) => {
-        return first + second
-    })
-    currentArr2 = []
-    // prevent from continuing to play after hold value is pressed
-    isMyTurn = true
-    console.log('player2 holds the value')
+    console.log(`${player}holds the  value`)
 
 }
 
-// dice image
 
-// function diceImg() {
-//     let input = randomRoll()
-//     switch (input) {
-//         case 1:
-//             dice.src = "C:/Downloads/dice/dice1.png"
-//             break;
-//         case 2:
-//             dice.src = 'C:/Downloads/dice/dice2.png'
-//             break;
-//         case 3:
-//             dice.src = 'C:/Downloads/dice/dice3.png'
-//         case 4:
-//             dice.src = 'C:/Downloads/dice/dice4.png'
-//         case 5:
-//             dice.src = 'C:/Downloads/dice/dice5.png'
-//         case 6:
-//             dice.src = 'C:/Downloads/dice/dice6.png'
-//     }
-// }
 
-// define the dice image path and use it to update diceImg source
+function winCondition(player, opponent) {
+    if (player.totalArr.reduce((first, second) => { return first + second }) >= 100) {
+        isGameOver = true;
+        player.totalScore.classList.add('winner')
+        opponent.totalScore.classList.add('loser')
+        message.textContent = 'Player 1 wins'
+    }
+    else if (opponent.totalArr.reduce((first, second) => { return first + second }) >= 100) {
+        isGameOver = true;
+        opponent.totalScore.classList.add('winner')
+        player.totalScore.classList.add('loser')
+        message.textContent = 'Player 2 Wins'
+    }
+}
 
-let diceInput = 'C:/Downloads/dice/dice' + randomRoll() + '.png'
+//  dice image randomizer
 
 
 
-// button event listeners
+// button event listener
+//function reset to reset currentScore when rolling a 1
 
 rollDice.addEventListener('click', function () {
-    if (totalArr1 >= 10 || totalArr2 >= 10) {
-        if (totalArr1 >= 10) {
-            totalScore1.classList.add('winner')
-            totalScore2.classList.add('loser')
-            message.textContent = 'player 1 wins'
+    if (isMyTurn === true && !isGameOver) {
+        updateScore(player1, player2)
+        reset(player1, player2)
+        dice.setAttribute('src', 'assets/dice' + randomRoll() + '.png')
 
-        } else {
-            totalScore2.classList.add('winner')
-            totalScore1.classList.add('loser')
-            message.textContent = 'player 2 wins'
-        }
-        isGameOver = true;
+
     }
-    else if (isMyTurn === true && !isGameOver) {
-        dice.setAttribute('src', diceInput)
-        updateScore()
-        console.log(randomRoll())
-    }
-    // problem with currentScore2 updating right after player1 rolls a 1
-    // changed to else if insted of nested if
     else if (isMyTurn === false && !isGameOver) {
-        dice.setAttribute('src', diceInput)
-        udpateScore2()
-        console.log(randomRoll())
-    }
+        updateScore(player2, player1)
+        reset(player2, player1)
+        dice.setAttribute('src', 'assets/dice' + randomRoll() + '.png')
 
+
+    }
+    winCondition(player1, player2)
 })
 
-holdValue.addEventListener('click', function () {
+// holdValue event listener
+
+holdValueBtn.addEventListener('click', function () {
     if (isMyTurn && !isGameOver) {
-        holdValue1()
-    }
-    else if (!isMyTurn && !isGameOver) {
-        dice.setAttribute('src', diceInput)
-        holdValue2()
+        holdValue(player1, player2)
+        isMyTurn = false
+    } else if (!isMyTurn && !isGameOver) {
+        holdValue(player2, player1)
         isMyTurn = true
     }
 })
 
 
 
+function newGame(player, opponent) {
+    console.log('clicked')
+    player.currentScore.textContent = 0
+    player.totalScore.textContent = 0
+    player.currentArr = []
+    player.totalArr = []
+    opponent.currentScore.textContent = 0
+    opponent.totalScore.textContent = 0
+    opponent.currentArr = []
+    opponent.totalArr = []
+    isMyTurn = true
+    isGameOver = false
+    player.totalScore.classList.remove('winner')
+    player.totalScore.classList.remove('loser')
+    opponent.totalScore.classList.remove('winner')
+    opponent.totalScore.classList.remove('loser')
+    message.textContent = ''
+}
 
-
-
-
-
+newGameBtn.addEventListener('click', function () {
+    return newGame(player1, player2)
+})
